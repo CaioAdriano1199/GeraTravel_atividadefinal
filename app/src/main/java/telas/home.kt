3f7165c09c57a8f09d9230e8a6f3e0d20b5b2f23
CaioAdriano1199 <caioadriano1199@gmail.com>
2026-04-30 21:19:57 -0300

    fazendo main

--- app/src/main/java/telas/home.kt
+++ app/src/main/java/telas/home.kt
@@ -1,15 +1,63 @@
 package telas
 
+import android.R.attr.shape
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Dehaze
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TelaHome(email: String){
-    Column() {
-        Text("Home")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Row(horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(bottom = 20.dp)
+                    ) {
+                        Button(onClick = { }, modifier = Modifier.background(color = Color.Transparent),
+                            ) {
+                            Icon(
+                                imageVector = Icons.Filled.Dehaze,
+                                contentDescription = "MenuButton",
+                                tint = Color.White,
+                                modifier = Modifier.size(33.dp)
+                                        Modifier.border(shape=CircleShape)
 
-        Text("E-mail = ${email}")
+                            )
+                        }
+                        Text(text = "Gera Travel", modifier = Modifier.padding(start = 10.dp))
+                    }
+
+                }
+            )
+        }
+    ){
+    paddingValues ->
+        Column(modifier = Modifier.padding(paddingValues)) { }
     }
-
 }
